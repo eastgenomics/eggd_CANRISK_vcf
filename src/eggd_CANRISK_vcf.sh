708978fb6ceb8376f12b6204a79b7e04d5c8703e
sophie22 <rzsozso@gmail.com>
2023-06-16 12:57:20 +0100

    remove unnecessary parts

--- src/eggd_CANRISK_vcf.sh
+++ src/eggd_CANRISK_vcf.sh
@@ -8,27 +8,10 @@ main() {
     # Download inputs from DNAnexus in parallel, these will be downloaded to /home/dnanexus/in/
     dx-download-all-inputs --parallel
 
-    ### PREPARE
-    mark-section "Preparing reference genome files"
-    # move reference genome files to /home/dnanexus/
-    mv $reference_fasta_path /home/dnanexus/ref_gen.fa.gz
-    mv $reference_fasta_index_path /home/dnanexus/ref_gen.fa.fai
-    # unpack reference genome fasta
-    gunzip /home/dnanexus/ref_gen.fa.gz
-
-    # # zip and fasta reference properly
-    # zcat $reference_fasta_path | bgzip -c > ref.bgz
-
-    # normalise/decompose VCF to split multi-allelics
-    mark-section "Normalising input VCF"
-    bcftools norm -m- -f ref_gen.fa -cs $sample_vcf_path > norm.vcf
-
-    # get sample name
-    sample_name=$(basename $sample_vcf_path | awk -F '-' '{ print $1"-"$2 }')
-    # TODO future proofing for Epic naming, split on "_" instead?
-
-
     ### PROCESS
+
+    # get full sample name
+    sample_name=$(basename $sample_vcf_path | awk -F '_' '{ print $1 }')
 
     ## 1. check CNV overlap:
     # if segments file input is provided, check if any PRS positions are in a CNV
@@ -63,87 +46,23 @@ main() {
 
     ## 2. check coverage:
     mark-section "Checking for low coverage"
-    # identify variants with low coverage (<20 DP)
-    # parse their coordinates to a bed file (bcftools query)
-    bcftools filter -i 'INFO/DP<20' norm.vcf | \
-        bcftools query -f '%CHROM\t%POS\t%INFO/END\n' > low_cov.bed
+    # identify variants with low coverage (below depth threshold input)
+    filter="FORMAT/DP<$depth"
+    bcftools filter -i $filter $sample_vcf_path > low_cov_PRS.vcf
 
-    # check whether any of the low covered positions overlap any PRS variants of interest
-    bedtools intersect -a $PRS_variants_path -b low_cov.bed > cov_intersect.bed
-
-    # write list of affected PRS positions to output file
-    echo "The following PRS positions are not covered to 20x:" > "$sample_name"_coverage_check.txt
-    # with header from PRS bed
-    grep "#" $PRS_variants_path >> "$sample_name"_coverage_check.txt
-    cat cov_intersect.bed >> "$sample_name"_coverage_check.txt
-    # TODO need some logic to check whether there are any
-    # and if so, add DP to each variant from VCF
-    # echo -e "# CHROM\tPOS\tDP" >> "$sample_name"_coverage_check.txt
+    # write list of affected PRS variants to output file
+    echo "The following PRS variants are not covered to $depth x:" > "$sample_name"_coverage_check.txt
+    echo -e "#CHROM\tPOS\tREF\tALT\tDP" >> "$sample_name"_coverage_check.txt
+    # write relevant info about affected variants from filtered VCF
+    bcftools query -f '%CHROM\t%POS\t%REF\t%ALT\t%DP\n' low_cov_PRS.vcf  >> "$sample_name"_coverage_check.txt
     echo -e "\nEnd of file" >> "$sample_name"_coverage_check.txt
 
 
     ## 3. convert VCF file:
-    mark-section "Converting input VCF"
-    # exclude low covered PRS variants?
-    bcftools filter -e 'INFO/DP<20' norm.vcf > norm_cov.vcf
-
-    # grab header
-    grep ^# norm_cov.vcf > "$sample_name"_canrisk_PRS.vcf
-
-    grep -v ^# $PRS_variants_path > PRS.bed
-
-    # make modified vcf
-    while read line; do
-        POSITION=$(printf '%s\t' $line | awk -F '\t' '{ print $1"\t"$3 }')
-        REF=$(printf '%s\t' $line | awk -F '\t' '{ print $4 }')
-        ALT=$(printf '%s\t' $line | awk -F '\t' '{ print $5 }')
-        # check position actually exists & warn if not (all positions should be present)
-        if ! grep -qP "^$POSITION\t" norm_cov.vcf; then
-            echo "ERROR: Position $POSITION not found in sample vcf - please ensure it has been formed correctly."
-            exit 1
-        fi
-        SAMPLE_LINE=$(grep -P "^$POSITION\t" norm_cov.vcf)
-        # get GT & DP indices
-        FORMAT=$(printf '%s\t' $SAMPLE_LINE | awk -F '\t' '{ print $9 }')
-        IFS=':' read -ra FIELD <<< "$FORMAT"
-        idx=1
-        for i in "${FIELD[@]}"; do
-            if [ $i == "GT" ]; then GT_INDEX=$idx
-            elif [ $i == "DP" ]; then DP_INDEX=$idx
-            fi
-            let "idx = idx + 1"
-        done
-        # get GT & DP values
-        FORMAT_VALUES=$(printf '%s\t' $SAMPLE_LINE | awk -F '\t' '{ print $10 }')
-        GENOTYPE=$(awk -v var="$GT_INDEX" -vt="${FORMAT_VALUES[*]}" 'BEGIN{n=split(t,a,":"); print a[var]}')
-        DEPTH=$(awk -v var="$DP_INDEX" -vt="${FORMAT_VALUES[*]}" 'BEGIN{n=split(t,a,":"); print a[var]}')
-        # # check depth is over 20x & record if not
-        # if [ $DEPTH -lt 20 ]; then
-        #     echo -e $POSITION "\t" $DEPTH >> "$sample_name"_coverage_check.txt
-        #     # TODO output all info about PRS from bed, include header
-        #     # TODO DITCH/remove/exclude records where depth < 20x
-        # fi
-        # check if grep finds the variant
-        if grep -qP "^$POSITION\t.*\t$REF\t$ALT\t" norm_cov.vcf; then
-            var=$(grep -P "^$POSITION\t.*\t$REF\t$ALT\t" norm_cov.vcf)
-            output=$(printf '%s\t' $var)
-            echo "variant is in norm_cov"
-            echo $output
-        # if not then check if genotype is 0/0 and add in PRS ALT
-        elif [ $GENOTYPE == '0/0' ]; then
-            output=$(printf '%s\t' $SAMPLE_LINE | awk -v var1="$REF" -v var2="$ALT" -F '\t' '{ print $1"\t"$2"\t"$3"\t"var1"\t"var2"\t"$6"\t"$7"\t"$8"\t"$9"\t"$10 }')
-            echo "variant is NOT in norm_cov, and has GT==0/0"
-            echo $output
-        # otherwise PRS is not present so we recode as 0/0
-        else
-            output=$(printf '%s\t' $SAMPLE_LINE | awk -v var1="$REF" -v var2="$ALT" -v var3="$DEPTH" -F '\t' '{ print $1"\t"$2"\t.\t"var1"\t"var2"\t.\t.\t.\tGT:DP\t0/0:"var3 }')
-            echo "variant is NOT in norm_cov, and NOT GT==0/0"
-            echo "PRS variant not found in sample VCF. Adding line to say sample is 0/0 at this position."
-            echo $output
-        fi
-        # print relevant lines to output file
-        printf '%s\t%s\t%s\t%s\t%s\t%s\t%s\t%s\t%s\t%s\n' $output >> "$sample_name"_canrisk_PRS.vcf
-    done < PRS.bed
+    mark-section "Filtering PRS VCF"
+    # exclude low covered PRS variants
+    bcftools filter -e $filter $sample_vcf_path > "$sample_name"_canrisk_PRS.vcf
+    grep -v ^# "$sample_name"_canrisk_PRS.vcf | wc -l
 
     ### OUTPUTS
     mark-section "Uploading output files"
